8045976fd441b917782456f393adcf56c42585f5
sanusathyan <119839627+sanusathyan@users.noreply.github.com>
2022-12-07 16:41:51 +0530

    Add files via upload

---
+++ AnonymousAction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AnonymousAction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int, int> Add = delegate (int firstnum, int secondnum)
+             {
+                 Console.WriteLine($"The sum is : {firstnum + secondnum}");
+             };
+
+            Action<int, int> Diff = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine($"The difference is : {firstnum - secondnum}");
+            };
+
+            Action<int, int> Mul = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine($"The product is : {firstnum * secondnum}");
+            };
+
+            Action<int, int> Div = delegate (int firstnum, int secondnum)
+            {
+                Console.WriteLine($"The quotient is : {firstnum / secondnum}");
+            };
+
+            Add(10, 20);
+            Diff(50, 20);
+            Mul(10, 20);
+            Div(100, 20);
+        }
+        
+    }
+}
---
+++ AnonymousFunc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace AnonymousMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, int, int> Add = delegate (int firstNum, int secondNum)
+              {
+                  return firstNum + secondNum;         
+              };
+
+            Func<int, int, int> Diff = delegate (int firstNum, int secondNum)
+            {
+                return firstNum - secondNum;
+            };
+
+            Func<int, int, int> Mul = delegate (int firstNum, int secondNum)
+            {
+                return firstNum * secondNum;
+            };
+
+            Func<int, int, int> Div = delegate (int firstNum, int secondNum)
+            {
+                return firstNum / secondNum;
+            };
+
+            Console.WriteLine(Add(10,20));
+            Console.WriteLine(Diff(40,20));
+            Console.WriteLine(Mul(80,20));
+            Console.WriteLine(Div(40,20));
+            
+        }
+    }
+}
